d73ee264561e10660ca65f578a87fed859c39232
SC-One <heydar.mahmoodi75@gmail.com>
2023-02-28 15:12:50 +0330

    simple example provided and build successfull

--- main.cpp
+++ main.cpp
@@ -1,3 +1,50 @@
-#include <filesystem>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) { return 0; }
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>  // for ScreenInteractive
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/node.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <string>
+
+int main(int argc, const char* argv[]) {
+  using namespace ftxui;
+  auto make_box = [](int dimx, int dimy) {
+    std::string title = std::to_string(dimx) + "x" + std::to_string(dimy);
+    return window(text(title) | hcenter | bold,
+                  text("content") | hcenter | dim) |
+           size(WIDTH, EQUAL, dimx) | size(HEIGHT, EQUAL, dimy);
+  };
+
+  auto style = size(WIDTH, GREATER_THAN, 20) | border |
+               size(HEIGHT, GREATER_THAN, 30) | size(WIDTH, LESS_THAN, 50);
+
+  auto document = hflow({
+                      make_box(7, 7),
+                      make_box(7, 5),
+                      make_box(5, 7),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(10, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(11, 4),
+                      make_box(12, 4),
+                      make_box(12, 5),
+                      make_box(12, 4),
+                      make_box(13, 4),
+                      make_box(13, 3),
+                      make_box(13, 3),
+                      make_box(10, 3),
+                  }) |
+                  style;
+
+  auto renderer = Renderer([&document]() { return document; });
+
+  auto screen = ScreenInteractive::Fullscreen();
+  screen.Loop(renderer);
+
+  return 0;
+}
